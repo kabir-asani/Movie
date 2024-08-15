f1439e28fe2c1e36c02c44cc363f86b5bd3f921e
Kabir Asani <iamkabirasani@gmail.com>
2024-08-16 01:03:53 +0530

    Set up project

---
+++ Movie/ViewControllers/HomeViewController/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewController.swift
+//  Movie
+//
+//  Created by Kabir Asani on 16/08/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
