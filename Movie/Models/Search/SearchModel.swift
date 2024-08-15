cd05b590525e408c9ed431d563cae1abad8d25fc
Kabir Asani <iamkabirasani@gmail.com>
2024-08-16 03:41:43 +0530

    Remove SearchItemModel.swift file and merge it with SearchModel.swift

--- Movie/Models/Search/SearchModel.swift
+++ Movie/Models/Search/SearchModel.swift
@@ -8,9 +8,58 @@
 import Foundation
 
 struct SearchModel: Codable {
-	let matches: [SearchModel]
+	let matches: [SearchItemModel]
+	let numberOfPages: Int
+	
+	init(
+		matches: [SearchItemModel],
+		numberOfPages: Int
+	) {
+		self.matches = matches
+		self.numberOfPages = numberOfPages
+	}
+	
+	init(
+		from decoder: Decoder
+	) throws {
+		let container = try decoder.container(
+			keyedBy: CodingKeys.self
+		)
+		self.matches = try container.decode(
+			[SearchItemModel].self, 
+			forKey: .matches
+		)
+		
+		let totalResults = try container.decode(
+			String.self,
+			forKey: .numberOfPages
+		)
+		
+		if let numberOfPages = Int(totalResults) {
+			self.numberOfPages = numberOfPages
+		} else {
+			throw DecodingError.dataCorruptedError(
+				forKey: .numberOfPages, 
+				in: container, 
+				debugDescription: "`totalResults` should be an Int."
+			)
+		}
+	}
 	
 	enum CodingKeys: String, CodingKey {
 		case matches = "Search"
+		case numberOfPages = "totalResults"
+	}
+}
+
+struct SearchItemModel: Identifiable, Codable {
+	let id: String
+	let title: String
+	let poster: URL
+	
+	enum CodingKeys: String, CodingKey {
+		case id = "imdbID"
+		case title = "Title"
+		case poster = "Poster"
 	}
 }
